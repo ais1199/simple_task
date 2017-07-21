6a5e3b7f37e55aaa1d9a131c2b6c15b9cc532969
Ivan Abaimov <ais1199@yandex.ru>
2017-07-21 13:28:11 +0300

    Разбираюсь с виджетом таблицы. Реализую открытие фаила и запись его в таблицу. Размер окна фиксирован, чтобы не было лишних проблем. Проблемы: не нашел, как сделать таблицу не редактируемой.

--- widget.cpp
+++ widget.cpp
@@ -1,13 +1,42 @@
 #include "widget.h"
 #include "ui_widget.h"
 
+#include <QDialog>
+#include <QFileDialog>
+#include <QFile>
+#include <QPushButton>
+
 Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget)
 {
     ui->setupUi(this);
-    setMaximumSize(600,400);
+    setMaximumSize(450,300);
+    setMinimumSize(450,300);
     setWindowTitle(QString("FReader"));
+    //ui->tab->setReadOnly(true);
+    connect(ui->open,&QPushButton::clicked,this,&Widget::upd);
+
+    NP();
+}
+void Widget::NP()
+{
+    ui->stat->setText(QString("No Problems"));
+}
+void Widget::upd()
+{
+    /*QString FN=QFileDialog::getOpenFileName(this,"open","",tr("Text files(*.txt)"));
+    QFile f(FN);
+    char buf[128];
+    if(f.open(QIODevice::ReadOnly))
+    {
+        while(!f.atEnd())
+        {
+            f.readLine(buf,128);
+        }
+        NP();
+    }
+    f.close(); */
 }
 
 Widget::~Widget()
--- widget.h
+++ widget.h
@@ -16,6 +16,8 @@ public:
     ~Widget();
 
 private:
+    void NP();
+    void upd();
     Ui::Widget *ui;
 };
 
